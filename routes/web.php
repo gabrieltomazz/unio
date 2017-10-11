07bb93c4d0a3a3f069f9dac191497bd500543e35
Unknown <yuricda96@gmail.com>
2017-10-11 20:52:34 -0300

    mudancas nas views

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', function () {
+    return view('login');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');

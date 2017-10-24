242e716f59e4c9133771b9d4aa0c0c95bc4bc130
Unknown <yuricda96@gmail.com>
2017-10-24 03:01:25 -0200

    index arrumado

--- resources/views/projetos/index.blade.php
+++ resources/views/projetos/index.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Projetos</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container">
+@extends('base.layout')
 
+
+
+@section('content')
 <nav class="navbar navbar-inverse">
     <div class="navbar-header">
         <a class="navbar-brand" href="{{ URL::to('nerds') }}">Projetos de Pesquisa</a>
@@ -17,53 +13,66 @@
     </ul>
 </nav>
 
-<h1>Todos os projetos</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
 
-<!-- will be used to show any messages -->
-@if (Session::has('message'))
-    <div class="alert alert-info">{{ Session::get('message') }}</div>
-@endif
+            <div class="panel panel-default">
 
-<table class="table table-striped table-bordered">
-    <thead>
-        <tr>
-            <td>Título</td>
-            <td>Apresentação</td>
-            <td>Objetivo</td>
-            <td>id</td>
-        </tr>
-    </thead>
-    <tbody>
-    @foreach($projetos as $key => $value)
-        <tr>
-            <td>{{ $value->titulo }}</td>
-            <td>{{ $value->apresentacao_do_problema }}</td>
-            <td>{{ $value->objetivo }}</td>
-            <td>{{ $value->id}}</td>
-         
+                <div class="panel-heading">
+                   <h1>Todos os projetos</h1>
+                </div>
+                <div class="panel-body">
+                    
 
-            <!-- we will also add show, edit, and delete buttons -->
-            <td>
+                    <!-- will be used to show any messages -->
+                    @if (Session::has('message'))
+                        <div class="alert alert-info">{{ Session::get('message') }}</div>
+                    @endif
 
-                <!-- delete the nerd (uses the destroy method DESTROY /nerds/{id} -->
-                <!-- we will add this later since its a little more complicated than the other two buttons -->
-                {{ Form::open(array('url' => 'projetos/' . $value->id, 'class' => 'pull-right')) }}
-                    {{ Form::hidden('_method', 'DELETE') }}
-                    {{ Form::submit('Deletar Projeto', array('class' => 'btn btn-warning')) }}
-                {{ Form::close() }}
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <td>Título</td>
+                                <td>Apresentação</td>
+                                <td>Objetivo</td>
+                                <td>id</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($projetos as $key => $value)
+                                <tr>
+                                    <td>{{ $value->titulo }}</td>
+                                    <td>{{ $value->apresentacao_do_problema }}</td>
+                                    <td>{{ $value->objetivo }}</td>
+                                    <td>{{ $value->id}}</td>
+                                 
 
-                <!-- show the nerd (uses the show method found at GET /nerds/{id} -->
-                <a class="btn btn-small btn-success" href="{{ URL::to('projetos/' . $value->id) }}">Ver detalhes sobre o projeto</a>
+                                    <!-- we will also add show, edit, and delete buttons -->
+                                    <td>
 
-                <!-- edit this nerd (uses the edit method found at GET /nerds/{id}/edit -->
-                <a class="btn btn-small btn-info" href="{{ URL::to('projetos/' . $value->id . '/edit') }}">Editar projeto</a>
+                                        <!-- delete the nerd (uses the destroy method DESTROY /nerds/{id} -->
+                                        <!-- we will add this later since its a little more complicated than the other two buttons -->
+                                        {{ Form::open(array('url' => 'projetos/' . $value->id, 'class' => 'pull-right')) }}
+                                            {{ Form::hidden('_method', 'DELETE') }}
+                                            {{ Form::submit('Deletar Projeto', array('class' => 'btn btn-warning')) }}
+                                        {{ Form::close() }}
 
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+                                        <!-- show the nerd (uses the show method found at GET /nerds/{id} -->
+                                        <a class="btn btn-small btn-success" href="{{ URL::to('projetos/' . $value->id) }}">Ver detalhes sobre o projeto</a>
 
+                                        <!-- edit this nerd (uses the edit method found at GET /nerds/{id}/edit -->
+                                        <a class="btn btn-small btn-info" href="{{ URL::to('projetos/' . $value->id . '/edit') }}">Editar projeto</a>
+
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-</body>
-</html>
+
+@endsection

16667f9e78ca3c4f01a59ca4b24cb99651bf7a15
Yuri Castro <yuricda96@gmail.com>
2017-11-08 21:36:16 -0200

    Tela Inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,43 +8,38 @@ $universidades = UniversidadeController::getListUniversidades()?>
 @section('header')
 
   <link rel="stylesheet" href="<?php echo asset('css/welcome.css')?>">
-  
+
 @endsecion
 
 @section('content')
   @include('base.navbar')
-  <div id = "fundo">
+  
 
   <div class="row vertical-offset-100">
-
-    <div class="centro">
-      <div> 
+    <div id="textintro" class="col-md-9 col-md-offset-2">
         <h1>Unio</h1>
         <h3>Integrando alunos e professores</h3>
-
-        
+    </div>
+      
+    <div class="centro">
+        <h4> Podemos começar?</h4>
         <div class="form-group"  id ="form-control-universidade">
             {{ Form::select('universidade_id', $universidades, Input::old('Universidade'), array('class' => 'form-control')) }}
-
         </div>
 
         <select  id = "form-control" class="form-control">
           <option>Aluno</option>
           <option>Professor</option>
         </select>
+
+        <button  type="button" class="button" >
+          <span>Entrar</span>
+        </button>
         {{ Form::close() }}  
-
-      </div>
-
-      <button id= "botao-entrar" type="button" class="btn btn-primary btn-lg">Entrar</button>
-
+      
     </div>
-
-
-
   </div>
 
-  </div>
 @endsection
 
 

d2521b06665a6c044c75aff7213051e99da5f811
Unknown <dan.apn7@gmail.com>
2017-11-25 23:17:04 -0200

    Testes de navegação
    Algumas rotas não estão retornando 200.

---
+++ tests/Feature/TelaTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TelaTest extends TestCase
+{
+   
+   public function test_create_user_using_form()
+   {
+   		 //$this->get('/')->assertStatus(200);
+   		 $this->get('/register')->assertStatus(200);
+   		 $this->get('/login')->assertStatus(200);
+   		 //$this->get('/logout')->assertStatus(200);
+   		 $this->get('/alunos')->assertStatus(200);
+   		 //$this->get('//projetos/departamento/{departamento_id}')->assertStatus(200);
+   		 //$this->get('/home')->assertStatus(200);
+
+   }
+}
